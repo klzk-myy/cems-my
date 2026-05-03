2d08b322f4fe1635546206e8296f6a585fb03c6a
CEMS Developer <cems@example.com>
2026-05-03 20:32:54 +0800

    fix: remove layouts namespace from Livewire config to fix view [app] error

--- config/livewire.php
+++ config/livewire.php
@@ -3,11 +3,6 @@
 return [
     'layout' => 'livewire.layouts.default',
 
-    'component_namespaces' => [
-        'layouts' => resource_path('views/livewire/layouts'),
-        'pages' => resource_path('views/livewire/pages'),
-    ],
-
     'comments' => [
         'enabled' => env('APP_ENV', 'production') === 'local',
     ],
