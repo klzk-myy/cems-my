6ef9f030b8e979c58b42784feed760b2463aff9b
CEMS Developer <cems@example.com>
2026-05-03 20:30:07 +0800

    fix: update Livewire config to remove layouts hint path and use livewire.layouts.default
    - Remove unused layouts namespace from component_namespaces
    - Component namespaced views are now only in livewire/layouts and livewire/pages
    - This fixes the 'No hint path defined for [layouts]' error

--- config/livewire.php
+++ config/livewire.php
@@ -3,6 +3,11 @@
 return [
     'layout' => 'livewire.layouts.default',
 
+    'component_namespaces' => [
+        'layouts' => resource_path('views/livewire/layouts'),
+        'pages' => resource_path('views/livewire/pages'),
+    ],
+
     'comments' => [
         'enabled' => env('APP_ENV', 'production') === 'local',
     ],
